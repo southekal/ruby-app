ddf89f6cdf5164789633d5dded4946fb8eff0467
Sandip Southekal <sandip.southekal@gifts.com>
2014-02-04 15:48:55 -0800

    updates

--- test-seo-checker.py
+++ test-seo-checker.py
@@ -3,17 +3,27 @@ import requests
 import argparse
 import time
 
-class TestSEOChecker():
+#class TestSEOChecker():
 
-	def testseoscript ():
-		servers = ['http://www.reddit.com']
+def test_seoscript ():
+	servers = ['http://www.reddit.com']
 
-		for server in servers:
-			r = requests.get(server)
-			soup = BeautifulSoup(r.text)
+	for server in servers:
+		r = requests.get(server)
+		soup = BeautifulSoup(r.text)
 
-			print 'Header H1:', soup.h1
-			print 'Header H2:', soup.h2
+		print soup.h1
+		print soup.h2
+		assert soup.h1.text
+		assert soup.h2.text
 
-			for link in soup.find_all('a'):
-				print link.get('href')
+		for link in soup.find_all('a'):
+			print link.get('href')
+
+def test_evens():
+    for i in range(0, 5):
+        yield check_even, i, i*3
+
+def check_even(n, nn):
+	print n % 2, nn % 2
+	assert n % 2 == 0 or nn % 2 == 0
