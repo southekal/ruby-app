df647c0deac61ffe9db05e32028def009b297e2e
Sandip Southekal <sandip.southekal@gifts.com>
2014-02-04 10:53:12 -0800

    added requirements file

--- seoChecker.py
+++ seoChecker.py
@@ -4,11 +4,14 @@ import argparse
 import time
 
 
-servers = ['http://www.reddit.com/']
+servers = ['http://www.reddit.com']
 
 for server in servers:
 	r = requests.get(server)
 	soup = BeautifulSoup(r.text)
 
-	print 'Header H1:', soup.h1.text
-	print 'Header H2:', soup.h2.text
+	print 'Header H1:', soup.h1
+	print 'Header H2:', soup.h2
+
+	for link in soup.find_all('a'):
+		print link.get('href')
